7600ed9b125844b0c50229e70ec20ef6d1216a13
Liam <liam.swift@antler.co>
2022-04-26 16:03:46 +1000

    Add apollo client, remove users table

--- antler-onboarding/pages/_app.tsx
+++ antler-onboarding/pages/_app.tsx
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const client = new ApolloClient({
+    uri: "http://localhost:4000/v1/graphql",
+    cache: new InMemoryCache(),
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
