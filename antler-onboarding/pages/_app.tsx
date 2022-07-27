b0652d935b724505dcfa3f9bb119c14bf6729cb9
Samuel Huang <samuelhww@gmail.com>
2022-07-27 15:40:20 +0800

    chore: add charka ui library

--- antler-onboarding/pages/_app.tsx
+++ antler-onboarding/pages/_app.tsx
@@ -1,17 +1,20 @@
-import "../styles/globals.css";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import '../styles/globals.css';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }) {
-  const client = new ApolloClient({
-    uri: "http://localhost:4000/v1/graphql",
-    cache: new InMemoryCache(),
-  });
+	const client = new ApolloClient({
+		uri: 'http://localhost:4000/v1/graphql',
+		cache: new InMemoryCache(),
+	});
 
-  return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<ChakraProvider>
+				<Component {...pageProps} />
+			</ChakraProvider>
+		</ApolloProvider>
+	);
 }
 
 export default MyApp;
